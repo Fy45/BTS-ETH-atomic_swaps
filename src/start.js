e87f9585dd6762dc7cda0086f49c7a40217669ee
Yuan Fan <yfan@kth.se>
2019-09-02 14:14:19 +0200

    perhaps test

--- src/start.js
+++ src/start.js
@@ -45,16 +45,16 @@ async function main() {
       break
     case '4':
       let ethRecipient = await prompt('Enter your ETH receiver address: ')
-      let ethHtlcId = await prompt('Enter your ETH HTLC id: ')
+      let ethHtlc = await prompt('Enter your ETH HTLC id: ')
       let secret = await prompt('Enter your preimage: ')
       console.log("Resolving ETH HTLC... ");
-      await eth.resolveHTLC(ethRecipient, ethHtlcId, '0x' + Buffer.from(secret).toString('hex'))
+      await eth.resolveHTLC(ethRecipient, ethHtlc, '0x' + Buffer.from(secret).toString('hex'))
       break
     case '5':
       let btsSender = await prompt('Enter your BTS account name: ')
-      let btsHtlcId = await prompt('Enter BTS HTLC_id you want to extend: ')
+      let btsHtlc = await prompt('Enter BTS HTLC_id you want to extend: ')
       let Extratime = await prompt('Enter the extra time you need for contract (in seconds): ')
-      let output = await bts.extendHTLC(btsSender, btsHtlcId, Extratime);
+      let output = await bts.extendHTLC(btsSender, btsHtlc, Extratime);
       console.log(output);
       break
   }
